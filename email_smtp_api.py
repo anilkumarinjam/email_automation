70e0e1ad6ad70a290e46e5a3cb4a18ac367d3725
anilkumarinjam <injamanilchowdary@gmail.com>
2025-03-07 08:46:00 -0600

    port update

--- email_smtp_api.py
+++ email_smtp_api.py
@@ -52,4 +52,4 @@ def send_email():
         return jsonify({"status": "error", "message": str(e)})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=6000)
+    app.run(host='0.0.0.0', port=7000)
